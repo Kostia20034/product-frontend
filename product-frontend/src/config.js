ad2b24933a7e296d3f0560b8df59a24288958678
Kostia <rumkostia92@gmail.com>
2026-05-22 23:12:18 -0500

    fix online server

--- product-frontend/src/config.js
+++ product-frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+const API_URL = "https://product-api-production-949c.up.railway.app"
 export default API_URL
